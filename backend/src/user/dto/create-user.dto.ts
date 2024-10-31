5d46e50ec876f4bae6912936a4fdd777ceb72cef
shoddy42 <wkonings@student.codam.nl>
2024-10-31 20:56:53 +0100

    re-enabled name requirements

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,19 +1,18 @@
 import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 
-//todo: re-enable later.
 export class CreateUserDto {
-	@IsString()
-	@IsNotEmpty()
-	// @Length(3, 20)
-	username: string;
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 15)
+  username: string;
 
-	@IsString()
-	@IsNotEmpty()
-	// @IsEmail()
-	email: string;
+  @IsString()
+  @IsNotEmpty()
+  // @IsEmail()
+  email: string;
 
-	@IsString()
-	@IsNotEmpty()
-	// @Length(8, 50)
-	password: string;
+  @IsString()
+  @IsNotEmpty()
+  @Length(0, 50)
+  password: string;
 }
